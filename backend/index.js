0a4c566018cf834a867463238f53e0d22a2e5b90
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-07-09 22:53:07 +0530

    added routes to backend

---
+++ backend/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const cors=require("cors");
+const dbConnection = require("./db");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended:true}))
+
+//insert to db - endpoint
+app.post("/post/users",(req,res)=>{
+    const {name,email,phone}=req.body;
+    const sqlInsert="INSERT into  users(name,email,phone) VALUES(?,?,?)";
+    dbConnection.query(sqlInsert,[name,email,phone],(err,rows)=>{
+        if(err){ 
+            console.log(err);
+            res.send(err);
+        }
+        else{
+            res.send("inserted successfully")
+            console.log(rows);
+        }
+    })
+})
+
+//delete user from DB - endpoint
+app.delete("/delete/users/:id",(req,res)=>{
+    const {id}=req.params;
+    const sqlRemove="DELETE FROM  users WHERE id=?";
+    dbConnection.query(sqlRemove,id,(err,rows)=>{
+        if(err) console.log(err);
+        console.log(rows);
+    })
+})
+
+//get users from DB - enpoint
+app.get("/get/users",(req,res)=>{
+    const sqlGet="SELECT * FROM users";
+    dbConnection.query(sqlGet,(err,rows)=>{
+        if(err) console.log(err);
+        res.send(rows);
+    })
+})
+
+//get a user from DB - endpoint
+app.get("/get/users/:id",(req,res)=>{
+    const {id}=req.params;
+    const sqlGet="SELECT * FROM users WHERE id=?";
+    dbConnection.query(sqlGet,id,(err,rows)=>{
+        if(err) console.log(err);
+        res.send(rows);
+    })
+});
+
+//Update user to DB - endpoint
+app.put("/put/users/:id",(req,res)=>{
+    const {id}=req.params;
+    const {name,email,phone} = req.body;
+    const sqlUpdate="UPDATE users set name=?, email=?, phone=? WHERE id=?";
+    dbConnection.query(sqlUpdate,[name,email,phone,id],(err,rows)=>{
+        if(err) console.log(err);
+        res.send(rows);
+    })
+})
+
+//listen app
+app.listen(5000,()=>{
+    console.log("server is running on 5000");
+})
