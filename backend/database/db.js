aac7d2419df13bf64efa6e4480a9747b9010f5ce
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-07-10 10:48:17 +0530

    updated .env

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,7 +5,7 @@ const dbConnection = mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DB_NAME
 });
 
 dbConnection.connect((err)=>{
